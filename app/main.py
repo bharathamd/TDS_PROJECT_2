932c5567dbf406d1aed7b8f7be11ff908dc09b8a
bharathamd <68017695+bharathamd@users.noreply.github.com>
2025-04-02 09:34:54 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -91,4 +91,6 @@ async def debug_function(
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.getenv("PORT", 8000))
+    
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
